58c1653c11f9bb79692bbda139f284464fcbc29b
Gaurish Prakhar Saini <gaurishprakhar@gmail.com>
2021-06-10 16:15:16 +0530

    hello world page

---
+++ index.php
@@ -0,0 +1,18 @@
+<html>
+ <head>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+         <link rel="stylesheet" type="text/css" href="style.css">
+        <title>GAURISH SAINI</title>
+        <link rel="icon" href="https://coloredcow.com/wp-content/uploads/2017/03/favicon.png" sizes="32x32">
+    </head>
+    <body class="brown lighten-4">
+    <div class="container">
+        <div class="center-align">
+            <div><img src="https://avatars.githubusercontent.com/u/58362095?v=4" height="150px" width="150px" class="circle responsive-img"></div>
+            <div style="margin-top: 10px;"><img src="wave.gif" height="30px" width="30px"></div>
+            <span>Hello World for Web Programming !</span>
+        </div>
+    </div>
+    </body>
+</html>
