41afc2862b66c5a8d804cb4c613e054f4a586282
Gaurish Prakhar Saini <gaurishprakhar@gmail.com>
2021-06-16 00:25:03 +0530

    enhancement and database connection

---
+++ script.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $("span").hover(function(){
+        $(this).css("background-color", "brown");
+        }, function(){
+        $(this).css("background-color", "brown");
+    });
+});
